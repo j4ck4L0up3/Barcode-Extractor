6816684662e7edca6e5bd407c5960b7b77dca6f5
Jack Gaskins <jackgaskins.tech@gmail.com>
2025-06-12 12:12:32 -0400

    create basic selenium program, docker doesn't work though

---
+++ barcode_extractor.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+# set options
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+
+# initialize firefox driver
+driver = webdriver.Chrome(options=chrome_options)
+
+# open encoder page
+driver.get('https://graphicore.github.io/librebarcode/documentation/code128.html')
+
+input_text = 'Yippee!'
+input_field = driver.find_element(By.CLASS_NAME, 'code128-encoder_input')
+
+input_field.clear()
+input_field.send_keys(input_text)
+
+time.sleep(1)
+
+output_field = driver.find_element(By.CLASS_NAME, "code128-encoder_output")
+output_text = output_field.get_attribute('value')
+
+print(output_text)
+
+# wait 2 seconds
+time.sleep(2)
+
+# close browser window
+driver.quit()
