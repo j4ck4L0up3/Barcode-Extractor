d738b3f6d4f57e0223c634b4069fb7bbef35d95f
Jack Gaskins <jackgaskins.tech@gmail.com>
2025-06-27 18:53:02 -0400

    delete Dockerfile, change extractor

--- barcode_extractor.py
+++ barcode_extractor.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+#from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 
 # set options
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
+# chrome_options = Options()
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--no-sandbox')
 
-# initialize firefox driver
-driver = webdriver.Chrome(options=chrome_options)
+# initialize chrome driver
+# uncomment stuff for options
+# driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome()
 
 # open encoder page
 driver.get('https://graphicore.github.io/librebarcode/documentation/code128.html')
