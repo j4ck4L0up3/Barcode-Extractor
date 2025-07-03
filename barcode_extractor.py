537472fb237cb265a1cd13299841ee8fbf9e63e3
Jack Gaskins <jackgaskins.tech@gmail.com>
2025-07-03 13:49:09 -0400

    Revert "finish barcode generator"
    This reverts commit 8b8d89a7862da40e5b28d3b9dba14b57c7f2b290.

--- barcode_extractor.py
+++ barcode_extractor.py
@@ -1,50 +1,38 @@
 #!/usr/bin/env python3
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+#from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
-import csv
 
 # set options
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
+# chrome_options = Options()
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--no-sandbox')
 
 # initialize chrome driver
-service = Service('/usr/bin/chromedriver')
-driver = webdriver.Chrome(service=service, options=chrome_options)
+# uncomment stuff for options
+# driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome()
 
 # open encoder page
 driver.get('https://graphicore.github.io/librebarcode/documentation/code128.html')
 
-input_file = 'inventory.csv'
-output_file = 'barcodes.txt'
+input_text = 'Yippee!'
+input_field = driver.find_element(By.CLASS_NAME, 'code128-encoder_input')
 
-with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
-    csv_reader = csv.reader(infile, delimiter=',', quotechar='"')
-    line_count = 0
+input_field.clear()
+input_field.send_keys(input_text)
 
-    for row in csv_reader:
-        if line_count != 0:
+time.sleep(1)
 
-            input_text = ''.join(row)
-            input_field = driver.find_element(By.CLASS_NAME, 'code128-encoder_input')
+output_field = driver.find_element(By.CLASS_NAME, "code128-encoder_output")
+output_text = output_field.get_attribute('value')
 
-            input_field.clear()
-            input_field.send_keys(input_text)
+print(output_text)
 
-            time.sleep(1)
-
-            output_field = driver.find_element(By.CLASS_NAME, "code128-encoder_output")
-            output_text = output_field.get_attribute('value')
-
-            outfile.write(output_text + '\n')
-        
-        line_count += 1
-
-        # wait 2 seconds
-        time.sleep(2)
+# wait 2 seconds
+time.sleep(2)
 
 # close browser window
 driver.quit()
